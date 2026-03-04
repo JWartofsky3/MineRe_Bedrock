d433fefe9ce70b423e146d4eb312080b049ee523
Jacob Wartofsky <jwartofsky@gmail.com>
2026-03-03 23:45:24 -0500

    Monkey adjustments

--- MineRe_BP/src/entities/projectiles/IceCharge.ts
+++ MineRe_BP/src/entities/projectiles/IceCharge.ts
@@ -56,6 +56,7 @@ export class IceCharge extends BaseCustomEntity {
         return;
       }
     }
+    world.sendMessage("ice charge ticking");
     if (entity.isInWater) {
       freezeArea(entity.dimension, entity.location, FREEZE_AREA_OPTIONS);
       entity.remove();
